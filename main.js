9469df1565c8dcad02c266c33058ce3a1501f2b1
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2020-06-16 11:17:06 +0900

    スポット追加フォームが動作するようになった。

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(function() {
 
-    var URL = "https://i5iz16898f.execute-api.us-east-2.amazonaws.com/default/date_concierge/plan";
+    let URL = "https://i5iz16898f.execute-api.us-east-2.amazonaws.com/default/date_concierge";
 
     $("#replan").click(function() {
         $("#replan").attr("disabled", "disabled");
         console.log('call ajax.');
         $.ajax({
             type: "GET",
-            url: URL, 
+            url: URL + '/plan', 
             dataType: "json",
             error: function() {
                 console.log('ERROR!');
@@ -24,7 +24,40 @@ $(function() {
                 $('#alcohol_plan').val(res['alcohol']['name']);
                 
                 $("#replan").removeAttr("disabled");
+
             }
         });
     });
+
+    $("#new_spot_id").focusout(function(){
+        console.log('new_spot_id focusout');
+    });
+
+    $('#new_spot').click(function(){
+        var data = {
+            spot_id:  $('#new_spot_id').val(),
+            genre_id: $('#new_spot_genre').val(),
+            name:     $('#new_spot_name').val()
+        };
+        
+        console.log(data);
+
+        $.ajax({
+            url: URL,
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            error: function() {
+                console.log('ERROR!');
+            },
+            success: function(res) {
+                console.log('SUCCESS!')
+                console.log(res);
+                $('#new_spot_alert').text('新しいスポットを追加しました。');
+                $('#new_spot_alert').removeClass('d-none');
+            }
+        });
+    });
+
 });
