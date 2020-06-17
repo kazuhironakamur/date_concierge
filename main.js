70c469d9e50ed0a426c39ca8a3b345cdc1cdb8cb
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2020-06-17 11:14:26 +0900

    スポット削除処理を追加。なぜかconfirmがループするので修正が必要。

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ $(function() {
                 $('#new_spot_alert').removeClass('d-none');
             },
             success: function(res) {
-                console.log('SUCCESS!')
+                console.log('SUCCESS!');
                 console.log(res);
                 $('#new_spot_alert').text('新しいスポットを追加しました。');
                 $('#new_spot_alert').removeClass('d-none');
@@ -87,15 +87,58 @@ $(function() {
                     s += '<td>' + item.spot_id + '</td>'
                     s += '<td>' + item.name + '</td>'
                     s += '<td>'
-                    s += '<div id="edit_spot" for="' + item.spot_id + '" class="btn btn-primary mr-3"><i class="fas fa-edit pr-3"></i>編集</div>'
-                    s += '<div id="delete_spot" for="' + item.spot_id + '" class="btn btn-error"><i class="fas fa-trash-alt pr-3"></i>削除</div>'
+                    s += '<div id="edit_spot" accesskey="' + item.spot_id + '" class="btn btn-primary mr-3"><i class="fas fa-edit pr-3"></i>編集</div>'
+                    s += '<div id="delete_spot" accesskey="' + item.spot_id + '" class="btn btn-error"><i class="fas fa-trash-alt pr-3"></i>削除</div>'
                     s += '</td>'
                     s += '</tr>';
                     return s
                 }, '');
                 $('#spot_table tbody').html(html);
+
+                // スポット編集イベント
+                $(document).on("click", "#edit_spot", function() {
+                    console.log('edit spot.');
+
+                    let spot_id = $(this)[0].accessKey;
+                    console.log(spot_id);
+                });
+
+                // スポット削除イベント
+                $(document).on('click', '#delete_spot', function() {
+                    console.log('delete spot.');
+
+                    let spot_id = $(this)[0].accessKey;
+                    console.log(spot_id);
+
+                    if (confirm('本当に削除しますか？')) {
+                        console.log('delete!');
+
+                        let data = {
+                            spot_id: spot_id
+                        };
+
+                        console.log(data);
+
+                        $.ajax({
+                            url: URL,
+                            type: "DELETE",
+                            dataType: "json",
+                            data: JSON.stringify(data),
+                            contentType: 'application/json',
+                            error: function(xhr, status, error) {
+                                console.log('ERROR!');
+                            },
+                            success: function(res) {
+                                console.log('SUCCESS!');
+                            }
+                        });
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                });
             }
         });
     });
-
 });
